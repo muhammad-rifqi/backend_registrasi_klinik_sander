5771361f833b2d9fc6a03040e05486a178d39a4d
muhammad-rifqi <muhammad45rifki@gmail.com>
2026-06-02 14:21:47 +0700

    udpate

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -6,9 +6,17 @@
     </div>
 
     <ul class="menu">
-        <li class="active"><a href="/dashboard">🏠 Dashboard</a></li>
-        <li><a href="/patient">👥 List Patient</a></li>
-        <li><a href="/setting">⚙️ Account Settings</a></li>
+        <li class="{{ request()->is('dashboard') ? 'active' : '' }}">
+            <a href="/dashboard">🏠 Dashboard</a>
+        </li>
+
+        <li class="{{ request()->is('patient') ? 'active' : '' }}">
+            <a href="/patient">👥 List Patient</a>
+        </li>
+
+        <li class="{{ request()->is('setting') ? 'active' : '' }}">
+            <a href="/setting">⚙️ Account Settings</a>
+        </li>
         <li><a href="#" onclick="logoutAction()">🚪 Logout</a></li>
     </ul>
 </div>

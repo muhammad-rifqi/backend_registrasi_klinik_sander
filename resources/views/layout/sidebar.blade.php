235487249dee9c58fae394a992f413b0db3b71f9
muhammad-rifqi <muhammad45rifki@gmail.com>
2026-06-02 13:17:19 +0700

    update

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -7,8 +7,8 @@
 
     <ul class="menu">
         <li class="active"><a href="/dashboard">🏠 Dashboard</a></li>
-        <li><a href="/patient">👥 Patient</a></li>
-        <li><a href="/setting">⚙️ Settings</a></li>
+        <li><a href="/patient">👥 List Patient</a></li>
+        <li><a href="/setting">⚙️ Account Settings</a></li>
         <li><a href="#" onclick="logoutAction()">🚪 Logout</a></li>
     </ul>
 </div>
